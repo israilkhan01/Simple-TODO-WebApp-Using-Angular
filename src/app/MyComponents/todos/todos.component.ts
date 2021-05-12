3dd49964502b2dc8f00689b19b376e75d79c3b8d
israilkhan01 <israil.khan.a888@gmail.com>
2021-05-12 18:24:44 +0530

    add todo

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,47 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {Todo} from "../../Todo";
+import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  // localItem: string;
+  todos: Todo[];
+  constructor() {
+    // const userJson = localStorage.getItem('currentUser');
+    // this.currentUser = userJson !== null ? JSON.parse(userJson) : new User();
+    const localItem = localStorage.getItem('todos');
 
-  todos:Todo[];
-  constructor() { 
-    this.todos = [
-      {
-        sno:1,
-        title:"this is title",
-        desc:'Description',
-        active:true,
-      },
-      {
-        sno:2,
-        title:"this is title2",
-        desc:'Description',
-        active:true,
-      },
-      {
-        sno:3,
-        title:"this is title3",
-        desc:'Description',
-        active:true,
-      }
-    ]
+    if(localItem == null){
+      this.todos = [];
+    }else{
+      this.todos = JSON.parse(localItem);
+    }
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  deleteTodo(todo:Todo){
+  deleteTodo(todo: Todo) {
     console.log(todo);
     const index = this.todos.indexOf(todo);
-    this.todos.splice(index,1);
+    this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
-  addTodo(todo:Todo){
+  addTodo(todo: Todo) {
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
